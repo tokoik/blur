169f9c39889c68aa6b4684847a2971e5f30c8d2d
Kohe Tokoi <tokoik@gmail.com>
2012-06-27 00:33:03 +0900

    update dependency

--- blur.d
+++ blur.d
@@ -1,108 +1,17 @@
-Attribute.o: Attribute.cpp Attribute.h gg.h /usr/X11R6/include/GL/gl.h \
-  /usr/X11R6/include/GL/glext.h /usr/X11R6/include/GL/glu.h \
-  /usr/X11R6/include/GL/glx.h /usr/X11R6/include/X11/Xlib.h \
-  /usr/X11R6/include/X11/X.h /usr/X11R6/include/X11/Xfuncproto.h \
-  /usr/X11R6/include/X11/Xosdefs.h /usr/X11R6/include/X11/Xutil.h \
-  /usr/X11R6/include/X11/keysym.h /usr/X11R6/include/X11/keysymdef.h \
-  /usr/X11R6/include/GL/glxext.h /usr/X11R6/include/GL/glut.h
-Circle.o: Circle.cpp Circle.h Object.h Shape.h Shader.h Attribute.h gg.h \
-  /usr/X11R6/include/GL/gl.h /usr/X11R6/include/GL/glext.h \
-  /usr/X11R6/include/GL/glu.h /usr/X11R6/include/GL/glx.h \
-  /usr/X11R6/include/X11/Xlib.h /usr/X11R6/include/X11/X.h \
-  /usr/X11R6/include/X11/Xfuncproto.h /usr/X11R6/include/X11/Xosdefs.h \
-  /usr/X11R6/include/X11/Xutil.h /usr/X11R6/include/X11/keysym.h \
-  /usr/X11R6/include/X11/keysymdef.h /usr/X11R6/include/GL/glxext.h \
-  /usr/X11R6/include/GL/glut.h
-Matrix.o: Matrix.cpp Matrix.h gg.h /usr/X11R6/include/GL/gl.h \
-  /usr/X11R6/include/GL/glext.h /usr/X11R6/include/GL/glu.h \
-  /usr/X11R6/include/GL/glx.h /usr/X11R6/include/X11/Xlib.h \
-  /usr/X11R6/include/X11/X.h /usr/X11R6/include/X11/Xfuncproto.h \
-  /usr/X11R6/include/X11/Xosdefs.h /usr/X11R6/include/X11/Xutil.h \
-  /usr/X11R6/include/X11/keysym.h /usr/X11R6/include/X11/keysymdef.h \
-  /usr/X11R6/include/GL/glxext.h /usr/X11R6/include/GL/glut.h
-Obj.o: Obj.cpp Obj.h Object.h Shape.h Shader.h Attribute.h gg.h \
-  /usr/X11R6/include/GL/gl.h /usr/X11R6/include/GL/glext.h \
-  /usr/X11R6/include/GL/glu.h /usr/X11R6/include/GL/glx.h \
-  /usr/X11R6/include/X11/Xlib.h /usr/X11R6/include/X11/X.h \
-  /usr/X11R6/include/X11/Xfuncproto.h /usr/X11R6/include/X11/Xosdefs.h \
-  /usr/X11R6/include/X11/Xutil.h /usr/X11R6/include/X11/keysym.h \
-  /usr/X11R6/include/X11/keysymdef.h /usr/X11R6/include/GL/glxext.h \
-  /usr/X11R6/include/GL/glut.h
-Object.o: Object.cpp Object.h Shape.h Shader.h Attribute.h gg.h \
-  /usr/X11R6/include/GL/gl.h /usr/X11R6/include/GL/glext.h \
-  /usr/X11R6/include/GL/glu.h /usr/X11R6/include/GL/glx.h \
-  /usr/X11R6/include/X11/Xlib.h /usr/X11R6/include/X11/X.h \
-  /usr/X11R6/include/X11/Xfuncproto.h /usr/X11R6/include/X11/Xosdefs.h \
-  /usr/X11R6/include/X11/Xutil.h /usr/X11R6/include/X11/keysym.h \
-  /usr/X11R6/include/X11/keysymdef.h /usr/X11R6/include/GL/glxext.h \
-  /usr/X11R6/include/GL/glut.h
-Shader.o: Shader.cpp Shader.h Attribute.h gg.h /usr/X11R6/include/GL/gl.h \
-  /usr/X11R6/include/GL/glext.h /usr/X11R6/include/GL/glu.h \
-  /usr/X11R6/include/GL/glx.h /usr/X11R6/include/X11/Xlib.h \
-  /usr/X11R6/include/X11/X.h /usr/X11R6/include/X11/Xfuncproto.h \
-  /usr/X11R6/include/X11/Xosdefs.h /usr/X11R6/include/X11/Xutil.h \
-  /usr/X11R6/include/X11/keysym.h /usr/X11R6/include/X11/keysymdef.h \
-  /usr/X11R6/include/GL/glxext.h /usr/X11R6/include/GL/glut.h
-Shape.o: Shape.cpp Shape.h Shader.h Attribute.h gg.h \
-  /usr/X11R6/include/GL/gl.h /usr/X11R6/include/GL/glext.h \
-  /usr/X11R6/include/GL/glu.h /usr/X11R6/include/GL/glx.h \
-  /usr/X11R6/include/X11/Xlib.h /usr/X11R6/include/X11/X.h \
-  /usr/X11R6/include/X11/Xfuncproto.h /usr/X11R6/include/X11/Xosdefs.h \
-  /usr/X11R6/include/X11/Xutil.h /usr/X11R6/include/X11/keysym.h \
-  /usr/X11R6/include/X11/keysymdef.h /usr/X11R6/include/GL/glxext.h \
-  /usr/X11R6/include/GL/glut.h
+Attribute.o: Attribute.cpp Attribute.h gg.h
+Circle.o: Circle.cpp Circle.h Object.h Shape.h Shader.h Attribute.h gg.h
+Matrix.o: Matrix.cpp Matrix.h gg.h
+Obj.o: Obj.cpp Obj.h Object.h Shape.h Shader.h Attribute.h gg.h
+Object.o: Object.cpp Object.h Shape.h Shader.h Attribute.h gg.h
+Shader.o: Shader.cpp Shader.h Attribute.h gg.h
+Shape.o: Shape.cpp Shape.h Shader.h Attribute.h gg.h
 SimpleShader.o: SimpleShader.cpp SimpleShader.h Shader.h Attribute.h gg.h \
-  /usr/X11R6/include/GL/gl.h /usr/X11R6/include/GL/glext.h \
-  /usr/X11R6/include/GL/glu.h /usr/X11R6/include/GL/glx.h \
-  /usr/X11R6/include/X11/Xlib.h /usr/X11R6/include/X11/X.h \
-  /usr/X11R6/include/X11/Xfuncproto.h /usr/X11R6/include/X11/Xosdefs.h \
-  /usr/X11R6/include/X11/Xutil.h /usr/X11R6/include/X11/keysym.h \
-  /usr/X11R6/include/X11/keysymdef.h /usr/X11R6/include/GL/glxext.h \
-  /usr/X11R6/include/GL/glut.h Matrix.h
-Square.o: Square.cpp Square.h Shape.h Shader.h Attribute.h gg.h \
-  /usr/X11R6/include/GL/gl.h /usr/X11R6/include/GL/glext.h \
-  /usr/X11R6/include/GL/glu.h /usr/X11R6/include/GL/glx.h \
-  /usr/X11R6/include/X11/Xlib.h /usr/X11R6/include/X11/X.h \
-  /usr/X11R6/include/X11/Xfuncproto.h /usr/X11R6/include/X11/Xosdefs.h \
-  /usr/X11R6/include/X11/Xutil.h /usr/X11R6/include/X11/keysym.h \
-  /usr/X11R6/include/X11/keysymdef.h /usr/X11R6/include/GL/glxext.h \
-  /usr/X11R6/include/GL/glut.h
-Texture.o: Texture.cpp Texture.h Attribute.h gg.h \
-  /usr/X11R6/include/GL/gl.h /usr/X11R6/include/GL/glext.h \
-  /usr/X11R6/include/GL/glu.h /usr/X11R6/include/GL/glx.h \
-  /usr/X11R6/include/X11/Xlib.h /usr/X11R6/include/X11/X.h \
-  /usr/X11R6/include/X11/Xfuncproto.h /usr/X11R6/include/X11/Xosdefs.h \
-  /usr/X11R6/include/X11/Xutil.h /usr/X11R6/include/X11/keysym.h \
-  /usr/X11R6/include/X11/keysymdef.h /usr/X11R6/include/GL/glxext.h \
-  /usr/X11R6/include/GL/glut.h
+ Matrix.h
+Square.o: Square.cpp Square.h Shape.h Shader.h Attribute.h gg.h
+Texture.o: Texture.cpp Texture.h Attribute.h gg.h
 TileShader.o: TileShader.cpp TileShader.h SimpleShader.h Shader.h \
-  Attribute.h gg.h /usr/X11R6/include/GL/gl.h \
-  /usr/X11R6/include/GL/glext.h /usr/X11R6/include/GL/glu.h \
-  /usr/X11R6/include/GL/glx.h /usr/X11R6/include/X11/Xlib.h \
-  /usr/X11R6/include/X11/X.h /usr/X11R6/include/X11/Xfuncproto.h \
-  /usr/X11R6/include/X11/Xosdefs.h /usr/X11R6/include/X11/Xutil.h \
-  /usr/X11R6/include/X11/keysym.h /usr/X11R6/include/X11/keysymdef.h \
-  /usr/X11R6/include/GL/glxext.h /usr/X11R6/include/GL/glut.h Matrix.h
-Trackball.o: Trackball.cpp Trackball.h gg.h /usr/X11R6/include/GL/gl.h \
-  /usr/X11R6/include/GL/glext.h /usr/X11R6/include/GL/glu.h \
-  /usr/X11R6/include/GL/glx.h /usr/X11R6/include/X11/Xlib.h \
-  /usr/X11R6/include/X11/X.h /usr/X11R6/include/X11/Xfuncproto.h \
-  /usr/X11R6/include/X11/Xosdefs.h /usr/X11R6/include/X11/Xutil.h \
-  /usr/X11R6/include/X11/keysym.h /usr/X11R6/include/X11/keysymdef.h \
-  /usr/X11R6/include/GL/glxext.h /usr/X11R6/include/GL/glut.h
-gg.o: gg.cpp gg.h /usr/X11R6/include/GL/gl.h \
-  /usr/X11R6/include/GL/glext.h /usr/X11R6/include/GL/glu.h \
-  /usr/X11R6/include/GL/glx.h /usr/X11R6/include/X11/Xlib.h \
-  /usr/X11R6/include/X11/X.h /usr/X11R6/include/X11/Xfuncproto.h \
-  /usr/X11R6/include/X11/Xosdefs.h /usr/X11R6/include/X11/Xutil.h \
-  /usr/X11R6/include/X11/keysym.h /usr/X11R6/include/X11/keysymdef.h \
-  /usr/X11R6/include/GL/glxext.h /usr/X11R6/include/GL/glut.h
-main.o: main.cpp gg.h /usr/X11R6/include/GL/gl.h \
-  /usr/X11R6/include/GL/glext.h /usr/X11R6/include/GL/glu.h \
-  /usr/X11R6/include/GL/glx.h /usr/X11R6/include/X11/Xlib.h \
-  /usr/X11R6/include/X11/X.h /usr/X11R6/include/X11/Xfuncproto.h \
-  /usr/X11R6/include/X11/Xosdefs.h /usr/X11R6/include/X11/Xutil.h \
-  /usr/X11R6/include/X11/keysym.h /usr/X11R6/include/X11/keysymdef.h \
-  /usr/X11R6/include/GL/glxext.h /usr/X11R6/include/GL/glut.h \
-  Trackball.h Matrix.h Square.h Shape.h Shader.h Attribute.h Obj.h \
-  Object.h SimpleShader.h TileShader.h
+ Attribute.h gg.h Matrix.h
+Trackball.o: Trackball.cpp Trackball.h gg.h
+gg.o: gg.cpp gg.h
+main.o: main.cpp gg.h Trackball.h Matrix.h Square.h Shape.h Shader.h \
+ Attribute.h Obj.h Object.h SimpleShader.h TileShader.h
